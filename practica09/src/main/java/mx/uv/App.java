108c177ff83e2a57b64c6eb4de86c1f7fa63af85
FernandLP <eduardolozada801@gmail.com>
2023-10-19 12:55:00 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,13 +1,20 @@
 package mx.uv;
+import static spark.Spark.*;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        get("/", 
+            (request, response) -> "<h1>Hola mundo</h1>"
+        );
+        get("/ruta1", 
+            (request, response) -> "<h1>Adios mundo</h1>"
+        );
+        get("/ruta2", 
+            (request, response) -> "<h1>Whats up mundo</h1>"
+        );
+        get("/ruta3", 
+            (request, response) -> "{'alumno':'ytguyhij','matricula':'weret','carrera':'asdf'}"
+        );
     }
 }
